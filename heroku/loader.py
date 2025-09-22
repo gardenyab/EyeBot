ef41179820020a60d1d8275bf857f7a707c68b10
gardenyab <gardenyablochnoy@gmail.com>
2025-09-22 06:47:18 +0000

    update heroku/loader.py

--- heroku/loader.py
+++ heroku/loader.py
@@ -12,6 +12,9 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
+
+
+
 import asyncio
 import builtins
 import contextlib
